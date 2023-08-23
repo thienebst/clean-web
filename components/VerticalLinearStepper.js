bcbd87de0ec3b76ce1864877df34c44aea824fa4
thienebst <thien.pham@ebst.tech>
2023-08-23 15:55:56 +0700

    udpate

--- components/VerticalLinearStepper.js
+++ components/VerticalLinearStepper.js
@@ -10,7 +10,7 @@ import Typography from "@mui/material/Typography";
 
  
 
-export default function VerticalLinearStepper({steps}) {
+export default function VerticalLinearStepper({steps = []}) {
   const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
